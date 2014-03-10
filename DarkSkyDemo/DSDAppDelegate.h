da0b24c27729d90cc604d92a22c3aff6378365e5
Ben Packard <ben.packard@gmail.com>
2014-03-09 22:00:14 -0400

    Initial Commit

---
+++ DarkSkyDemo/DSDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSDAppDelegate.h
+//  DarkSkyDemo
+//
+//  Created by Ben Packard on 3/9/14.
+//  Copyright (c) 2014 Made in Bletchley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DarkSkyDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DarkSkyDemo
+//
+//  Created by Ben Packard on 3/9/14.
+//  Copyright (c) 2014 Made in Bletchley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DSDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSDAppDelegate class]));
+	}
+}
